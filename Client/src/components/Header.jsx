5ee688e6da45041ed134897e2cd95890cfbc84cf
Diego Ovando <c9sneaky222@hotmail.com>
2022-03-22 19:04:06 -0700

    Actualización NavBar
    Diseño corregido al item del Status del login del user

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     Axios.defaults.withCredentials = true;
 
     const logout = () => {
-        Axios.post('http://localhost:3001/logout' , {
+        Axios.post('http://localhost:3001/logout', {
         }).then((response) => {
             console.log(response);
         })
@@ -33,19 +33,16 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light ">
                 <div className="container-fluid py-2">
-                    
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <NavLink className="nav-link text-primary"   to="/">
-                        <div class="container" style={{"padding-left":"20px","text":"center", "font-family":"Arial, Helvetica, sans-serif", "color":"black","font-size":"25px"}}>
+                    <NavLink className="nav-link text-primary" to="/">
+                        <div class="container" style={{ "padding-left": "20px", "text": "center", "font-family": "Arial, Helvetica, sans-serif", "color": "black", "font-size": "25px" }}>
                             Hospital del celular
                         </div></NavLink>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {/* <li className="nav-item">
-                                <NavLink className="nav-link" to="/">Inicio</NavLink>
-                            </li> */}
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/products">Productos</NavLink>
                             </li>
@@ -56,35 +53,38 @@ const Header = () => {
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/contact">Citas</NavLink>
                             </li>
-                            
+
                             {rol == 'admin' && (
                                 <li classname="nav-item">
-                                <NavLink className="nav-link" to="/citas">Consultar</NavLink>
+                                    <NavLink className="nav-link" to="/citas">Consultar</NavLink>
                                 </li>
                             )}
-                            <li className="nav-item">
-                                {rol == '' ? 
-                                <p></p> :
-                                <div className="container-sm" style={{"padding-top":"6px","textAlign":"center"}}>
-                                    <p className="p-1 bg-primary text-white rounded glyphicon-user"><span className="fa fa-user me-1"></span>Usuario: {rol}</p>
-                                </div>
-                                }
-                            </li>
                         </ul>
-                    
-                    {rol == '' &&
-                    <>
-                    <Login/>
-                    <Signup/>
-                    </>
-                    }
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ "padding-top": "15px", "textAlign": "center", "height": "10px" }}>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    {rol == '' ?
+                                        <p></p> :
+                                        <div className="container-sm" >
+                                            <p className="p-1 bg-primary text-white rounded glyphicon-user"><span className="fa fa-user me-1"></span>Usuario: {rol}</p>
+                                        </div>
+                                    }
+                                </li>
+                            </ul>
+                        </div>
 
-                    
-                    <CartBtn/>
-                    
-                    {!rol == '' &&
-                    <button onClick={logout} className="btn btn-outline-primary ms-2"><span className="fa fa-sign-out me-1"></span>Cerrar sesión</button>
-                    }
+                        {rol == '' &&
+                            <>
+                                <Login />
+                                <Signup />
+                            </>
+                        }
+
+                        <CartBtn />
+
+                        {!rol == '' &&
+                            <button onClick={logout} className="btn btn-outline-primary ms-2"><span className="fa fa-sign-out me-1"></span>Cerrar sesión</button>
+                        }
                     </div>
                 </div>
             </nav>
